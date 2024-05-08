97bc2799eb2e0e231e54608073202f0eb489a87b
FPraise2023 <francis.guede2020@gmail.com>
2024-05-08 17:36:35 +0200

    :tada: Basis: Components created with their basic content.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Good Old-Fashioned Pancakes',
+      'Perfect pancakes are easier to make than you think. ',
+      'https://www.allrecipes.com/thmb/tOfkDV1xc28JMmITPQlQwy3FNWs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/21014-Good-old-Fashioned-Pancakes-mfs_001-1fa26bcdedc345f182537d95b6cf92d8.jpg'),
+
+    new Recipe('Good Old-Fashioned Pancakes',
+      'Perfect pancakes are easier to make than you think. ' +
+      'This pancake recipe produces thick, fluffy, and all-around delicious ' +
+      'pancakes with just a few ingredients that are probably already in your ' +
+      'kitchen (and it\'s so much better than the boxed stuff).',
+      'https://www.allrecipes.com/thmb/tOfkDV1xc28JMmITPQlQwy3FNWs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/21014-Good-old-Fashioned-Pancakes-mfs_001-1fa26bcdedc345f182537d95b6cf92d8.jpg'),
+
+    new Recipe('Pickled Egg Salad Sandwich',
+      'If you want to add a little zing to your egg salad, pickle your eggs first. ' +
+      'It makes a great twist on a classic and might just become your preferred egg salad sandwich.',
+      'https://www.allrecipes.com/thmb/9gVChRz5smugwtGt8MGtO2Ah4gI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/pickled-egg-salad-sandwich-2000-19f667434d354303a804773063abbfe7.jpg'),
+
+    new Recipe('Crispy Tater Caesar Salad',
+      'This crispy tater Caesar salad is for you if you love fries in a salad. ' +
+      'Crispy smashed tater tot croutons, a doctored-up bottled Caesar dressing, ' +
+      'and crispy chicken cutlets join Romaine lettuce and Parmesan in this meal in a bowl.',
+      'https://www.allrecipes.com/thmb/duInZUhawM-i0zRXtsKiZQ5MiRo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/8628372_Crispy-Tater-Caesar-Salad_Dotdash-Meredith-Food-Studios_4x3-660c08772b3d405abaa397597a5d8c17.jpg')
+  ];
+
+}
