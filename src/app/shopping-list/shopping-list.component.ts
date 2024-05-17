3d2730e71c84e5ef20bbb719b0e2d3a103eb9cd0
FPraise2023 <francis.guede2020@gmail.com>
2024-05-17 09:42:09 +0200

    :sparkles: Replacing EventEmitter by Subject, having in mind that Output EventEmitter must not be replaced by Subject.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "./shopping-list.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredientList: Ingredient[];
+  updateIngredientsSub: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit(): void {
     this.ingredientList = this.shoppingListService.getIngredients();
-    this.shoppingListService.updateIngredients.subscribe(
+    this.updateIngredientsSub = this.shoppingListService.updateIngredients.subscribe(
       () => this.shoppingListService.getIngredients()
     )
+  }
+
+  ngOnDestroy() {
+    this.updateIngredientsSub.unsubscribe();
   }
 
 
